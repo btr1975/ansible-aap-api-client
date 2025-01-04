90d45ba0a32ec447a43bf821e6ebe9dcd0fa6ea5
Ben Trachtenberg <e_ben_75-python@yahoo.com>
2025-01-04 16:32:09 -0600

    Change license date, added 3.13 testing, added support for 3.13, changed version to 0.1.2, changed build version to 3.10

--- ansible_aap_api_client/version.py
+++ ansible_aap_api_client/version.py
@@ -2,7 +2,7 @@
 Holds the version information for the package
 """
 
-__copyright__ = "Copyright (c) 2024 Benjamin P. Trachtenberg"
+__copyright__ = "Copyright (c) 2024-2025 Benjamin P. Trachtenberg"
 __status__ = "prod"
-__version_info__ = (0, 1, 1)
+__version_info__ = (0, 1, 2)
 __version__ = ".".join(map(str, __version_info__))
