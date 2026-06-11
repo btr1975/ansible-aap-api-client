b379a80c2ea982df76e843866e381583d331d187
Ben Trachtenberg <e_ben_75-python@yahoo.com>
2026-06-11 12:31:41 -0500

    adding missing imports

--- ansible_aap_api_client/__init__.py
+++ ansible_aap_api_client/__init__.py
@@ -6,7 +6,9 @@ from ansible_aap_api_client.ansible_aap_api_client_cli import cli
 from ansible_aap_api_client.async_aap_client import AsyncAAPClient
 from ansible_aap_api_client.aap_client import AAPClient
 from ansible_aap_api_client.inventory_management import InventoryBuilder
+from ansible_aap_api_client.async_inventory_management import AsyncInventoryBuilder
 from ansible_aap_api_client.job_management import JobManagement
+from ansible_aap_api_client.async_job_management import AsyncJobManagement
 from ansible_aap_api_client.schemas import (
     InventoryRequestSchema,
     InventoryHostRequestSchema,
